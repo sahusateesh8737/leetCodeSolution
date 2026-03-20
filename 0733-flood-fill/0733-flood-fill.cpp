4b757324583c063088eb1fb35c001ef4a14f6d89
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-03-20 15:01:39 +0530

    Time: 1 ms (20.92%), Space: 18.2 MB (55.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    void dfs(int sr , int sc , vector<vector<int>>&ans , int newcolor , int dx[] , int dy[] , int sourceColor){
-        ans[sr][sc] = newcolor;
+    void bfs(int sr, int sc, vector<vector<int>>& ans, int ncolor, int pcolor) {
         int n = ans.size();
         int m = ans[0].size();
+        queue<pair<int, int>> q;
 
-        for(int i =0;i<4;i++){
-            int newX = sr+ dx[i];
-            int newY = sc + dy[i];
-            if(newX >=0 && newX <n &&newY >=0 && newY <m  && ans[newX][newY] == sourceColor)
-            dfs(newX , newY , ans , newcolor ,dx , dy ,sourceColor );
+        q.push({sr, sc});
+        ans[sr][sc] = ncolor;
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] == pcolor) {
+                    ans[nx][ny] = ncolor; // Color it before pushing to prevent duplicate entries
+                    q.push({nx, ny});
+                }
+            }
         }
-}
+    }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int sourceColor = image[sr][sc];
-        vector<vector<int>>ans = image;
-        if (sourceColor == color) {
-            return image; 
-        }
+        int inColor = image[sr][sc];
+        
+    
+        if (inColor == color) return image;
 
-        int dx[] = {1 , -1 ,0 ,0};
-        int dy[] = {0 ,0 ,-1 ,1};
-
-        dfs(sr , sc , ans , color, dx , dy , sourceColor);
-        return ans;
+      
+        bfs(sr, sc, image, color, inColor);
+        
+        return image;
     }
 };
