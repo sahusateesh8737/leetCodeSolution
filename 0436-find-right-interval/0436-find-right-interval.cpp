081d74341eb5aeb4269f1ed9a2f3e53a234e11e1
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-04-08 09:41:56 +0530

    Time: 8 ms (67.69%), Space: 29.5 MB (72.25%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<int>ans;
-        for(int i = 0;i<n;i++){
-            int endInterval = intervals[i][1];
-            int minStartIndex = -1;
-            long long minimumVal = 2e6;
-            for(int j=0;j<n;j++){
-                if(intervals[j][0] >= endInterval){
-                    if(intervals[j][0] < minimumVal){
-                        minimumVal = intervals[j][0];
-                        minStartIndex = j;
-                    }
-                }
-            }
-            ans.push_back(minStartIndex);
-
+        vector<int>ans(n , -1);
+        vector<pair<int ,int>>starts;
+        for(int i=0;i<n;i++ ){
+            starts.push_back({intervals[i][0] , i});
         }
+        sort(starts.begin() , starts.end());
+
+        for (int i = 0; i < n; i++) {
+            int endVal = intervals[i][1];
+            
+      
+            auto it = lower_bound(starts.begin(), starts.end(), make_pair(endVal, -2147483648));
+            
+            if (it != starts.end()) {
+                ans[i] = it->second;
+            }
+        }
+        
         return ans;
     }
-
 };
