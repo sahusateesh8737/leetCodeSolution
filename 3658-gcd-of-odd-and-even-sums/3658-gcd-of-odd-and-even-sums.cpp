833ad23ac6a721ca7b5c3ba3d326c4bb02b1d0a5
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-07-15 13:48:25 +0530

    Time: 1 ms (21.15%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
-
-
-int gcde(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
     int gcdOfOddEvenSums(int n) {
-        int sumEven = 0;
-        int sumOdd = 0;
-        for(int i=1;i<=n*2;i++){
-            if(i%2 ==0){
-                sumEven = sumEven + i;
-            }
-            else {
-                sumOdd = sumOdd + i;
-            }
-        }
-       int gc =  gcde(sumEven,sumOdd);
-       return gc;
+        int evenSum = n * (n+1);
+        int odd = n * n;
+        return __gcd(evenSum , odd);
     }
 };
