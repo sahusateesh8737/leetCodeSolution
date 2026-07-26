afb48bab6572603d0cfd8cfc203b543795d33ca0
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-07-26 22:25:27 +0530

    Time: 9 ms (42.43%), Space: 31.5 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,16 +4,10 @@ class Solution {
 public:
     int maximumProduct(std::vector<int>& nums) {
         int n = nums.size();
-        // Sort the array to easily access smallest and largest elements
-        std::sort(nums.begin(), nums.end());
-        
-        // Option 1: Product of the three largest elements
+   
+        sort(nums.begin(), nums.end());
         int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
-        
-        // Option 2: Product of the two smallest elements and the largest element
         int option2 = nums[0] * nums[1] * nums[n - 1];
-        
-        // Return the maximum of the two possibilities
-        return std::max(option1, option2);
+        return max(option1, option2);
     }
 };
