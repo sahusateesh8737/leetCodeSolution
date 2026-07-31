ff97159a46764172fb830fb034829e7ad91a2ba9
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-07-31 12:13:49 +0530

    Time: 154 ms (5%), Space: 113.5 MB (9%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>st;
+        for(int i =0;i<nums.size();i++){
+            if(st.count(nums[i]) >0){
+                return true;
+            }
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
